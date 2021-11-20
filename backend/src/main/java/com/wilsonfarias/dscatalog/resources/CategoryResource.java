c93a55bb75a9dedbdd0ccf4acddb1c60b26c9c42
wilFarias <wilsonluiz.dsf@gmail.com>
2021-11-20 10:49:14 -0300

    Post new Category

--- backend/src/main/java/com/wilsonfarias/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/wilsonfarias/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.wilsonfarias.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wilsonfarias.dscatalog.dto.CategoryDTO;
 import com.wilsonfarias.dscatalog.services.CategoryService;
@@ -32,10 +36,23 @@ public class CategoryResource {
 		return ResponseEntity.ok().body(list);
 	}
 
-	/*PathVariable linka a variável utilizada no parâmetro do método com o id do endpoint*/	
+	/* @PathVariable linka a variável utilizada no parâmetro do método com o id do endpoint*/	
+	
 	@GetMapping(value = "/{id}") //Define o endpoint
 	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
 		CategoryDTO dto = service.findById(id);		
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	// Para se inserir um novo recurso, pelo padrão REST se utiliza um POST
+	// @RequestBody, linka o objeto da requisição com o objeto dto do método
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insertCategory(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		//Insere no cabeçalho da resposta HTTP o location do novo registro criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- backend/src/main/java/com/wilsonfarias/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/wilsonfarias/dscatalog/services/CategoryService.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import com.wilsonfarias.dscatalog.services.exceptions.EntityNotFoundException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.wilsonfarias.dscatalog.dto.CategoryDTO;
 import com.wilsonfarias.dscatalog.entities.Category;
 import com.wilsonfarias.dscatalog.repositories.CategoryRepository;
+import com.wilsonfarias.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -32,6 +31,14 @@ public class CategoryService {
 		Optional<Category> obj = repository.findById(id);
 		//Implementação que trata a exceção no caso de id não existente
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
 	
