cc6f637f86b2406d7d7526f47226c0b74f3c3728
wilFarias <wilsonluiz.dsf@gmail.com>
2023-06-24 17:32:00 -0300

    POST Category

--- backend/src/main/java/com/springproject/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/springproject/dscatalog/services/CategoryService.java
@@ -6,11 +6,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springproject.dscatalog.dto.CategoryDTO;
 import com.springproject.dscatalog.entities.Category;
 import com.springproject.dscatalog.repositories.CategoryRepository;
 import com.springproject.dscatalog.services.exceptions.EntityNotFoundException;
+
+
 
 @Service
 public class CategoryService {
@@ -18,6 +21,7 @@ public class CategoryService {
 	@Autowired //injecao de dependencia automatica do Spring
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll(){
 		List<Category> list = repository.findAll();
 		/* Para cada elemento do stream mapeado (que são tipo Category),
@@ -27,9 +31,20 @@ public class CategoryService {
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
+	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		Category categoryById = obj.orElseThrow(() -> new EntityNotFoundException("Categoria não encontrada"));
 		return new CategoryDTO(categoryById);
+	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		 Category category = new Category();
+		 category.setName(dto.getName());
+		 /*o método save, por padrão, retorna a entidade salva
+		  * ao final da transação*/
+		 category = repository.save(category);
+		 return new CategoryDTO(category);
 	}
 }
