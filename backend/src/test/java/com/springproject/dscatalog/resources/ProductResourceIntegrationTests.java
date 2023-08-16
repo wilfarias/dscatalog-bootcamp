17fbcf7487dacab50d2d4d15f7b47c9e5beebfe8
wilFarias <wilsonluiz.dsf@gmail.com>
2023-08-16 11:31:18 -0300

    Integration Tests ProductResource - update

--- backend/src/test/java/com/springproject/dscatalog/resources/ProductResourceIntegrationTests.java
+++ backend/src/test/java/com/springproject/dscatalog/resources/ProductResourceIntegrationTests.java
@@ -1,6 +1,7 @@
 package com.springproject.dscatalog.resources;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -8,12 +9,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springproject.dscatalog.dto.ProductDTO;
+import com.springproject.dscatalog.factories.Factory;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,9 +27,13 @@ public class ProductResourceIntegrationTests {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
 	private Long existingId;
 	private Long nonExistingId;
 	private Long countTotalProducts;
+	private ProductDTO productDTO;
 	
 	@BeforeEach
 	void setUp() throws Exception{
@@ -33,8 +41,38 @@ public class ProductResourceIntegrationTests {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
+		productDTO = Factory.createProductDTO();
 	}
 	
+	@Test
+	public void updateShouldReturnProductDTOWhenIdExists() throws Exception{
+		
+		String jsonProductDTO = objectMapper.writeValueAsString(productDTO);
+		
+		String expectedName = productDTO.getName();		
+		
+		ResultActions result = mockMvc.perform(put("/products/{id}", existingId)
+				.content(jsonProductDTO)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").value(existingId));
+		result.andExpect(jsonPath("$.name").value(expectedName));		
+	}
+	
+	@Test
+	public void updateShouldReturnNotFoundWhenIdDoesNotExist() throws Exception{
+
+		String jsonProductDTO = objectMapper.writeValueAsString(productDTO);	
+		
+		ResultActions result = mockMvc.perform(put("/products/{id}", nonExistingId)
+				.content(jsonProductDTO)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isNotFound());		
+	}	
 	
 	@Test
 	public void findAllShouldReturnSortedPageWhenSortByName() throws Exception{
