4c96b894ce0182ca99079dd52ad8127dab43ab21
wilFarias <wilsonluiz.dsf@gmail.com>
2023-08-16 09:22:42 -0300

    Integration Tests ProductService - findAll

--- backend/src/test/java/com/springproject/dscatalog/services/ProductServiceIntegrationTests.java
+++ backend/src/test/java/com/springproject/dscatalog/services/ProductServiceIntegrationTests.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.springproject.dscatalog.dto.ProductDTO;
 import com.springproject.dscatalog.repositories.ProductRepository;
 import com.springproject.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional //ao final de cada test o BD executa um rollback ao estado inicial
 public class ProductServiceIntegrationTests {
 	
 	@Autowired
@@ -28,6 +34,43 @@ public class ProductServiceIntegrationTests {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void findAllShouldReturnPage() {
+		
+		PageRequest page = PageRequest.of(0, 10);
+		
+		Page<ProductDTO> result = service.findAll(page);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertEquals(10, result.getSize());
+		
+	}
+	
+	@Test
+	public void findAllShouldReturnSortedPageWhenSortByName() {
+		
+		PageRequest page = PageRequest.of(0, 10, Sort.by("name"));
+		
+		Page<ProductDTO> result = service.findAll(page);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		
+	}
+	
+	@Test
+	public void findAllShouldReturnEmptyPageWhenPageDoesNotExist() {
+		
+		PageRequest page = PageRequest.of(50, 10);
+		
+		Page<ProductDTO> result = service.findAll(page);
+		
+		Assertions.assertTrue(result.isEmpty());
+		
 	}
 	
 	@Test
